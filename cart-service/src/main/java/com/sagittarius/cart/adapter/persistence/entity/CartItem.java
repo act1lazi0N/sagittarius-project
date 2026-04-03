b6afce394f78b752ff0eb10980ab54d13bf17f24
Kyle Mandison <chemkhien255@gmail.com>
2026-04-03 10:24:49 +0700

    Cart service: Initializing RedisConfig and Creating Cart Service

---
+++ cart-service/src/main/java/com/sagittarius/cart/adapter/persistence/entity/CartItem.java
@@ -0,0 +1,18 @@
+package com.sagittarius.cart.adapter.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    private String productId;
+    private Integer quantity;
+    private BigDecimal price;
+}
